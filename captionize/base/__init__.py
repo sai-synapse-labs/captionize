03a969cbfd574a00698a834535c99bceab2ff1bc
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-03-22 16:43:30 +0530

    Refactor

---
+++ captionize/base/__init__.py
@@ -0,0 +1,3 @@
+from .miner import BaseMinerNeuron
+from .neuron import BaseNeuron
+from .validator import BaseValidatorNeuron
--- captionize/validator/__init__.py
+++ captionize/validator/__init__.py
@@ -1,3 +1,2 @@
-# from .forward import forward
 from .reward import get_rewards
 from .generate import generate_synthetic_jobs
